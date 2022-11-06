2111638779fa2da3fcc8d7387691fd2e16e5dfd4
anhttq <quynhanh1999tsth@gmail.com>
2022-11-06 10:51:39 +0700

    submit SQL homework

---
+++ SQLforQC/SQLPractice_QuynhAnh.sql
@@ -0,0 +1,111 @@
+USE SQLforQC
+GO
+
+--question 1
+SELECT department_id FROM employees WHERE last_name = 'Zlotkey';
+SELECT last_name, hire_date FROM employees WHERE department_id = 80 AND last_name != 'Zlotkey';
+
+SELECT last_name, hire_date FROM employees
+WHERE department_id = 
+(SELECT department_id FROM employees WHERE last_name = 'Zlotkey') 
+AND last_name != 'Zlotkey';
+
+SELECT e1.last_name, e1.hire_date FROM employees e1
+LEFT JOIN employees e2 ON e1.department_id = e2.department_id
+WHERE e2.last_name = 'Zlotkey' AND e1.last_name != 'Zlotkey';
+
+--question 2
+SELECT employee_id, last_name, salary FROM employees
+WHERE salary >
+(SELECT AVG(salary) FROM employees)
+ORDER BY salary ASC;
+
+--question 3
+SELECT employee_id, last_name FROM employees
+WHERE department_id IN
+(SELECT department_id FROM employees WHERE last_name LIKE '%u%');
+
+--question 4
+SELECT last_name, e.department_id, job_id FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE d.location_id = 1700;
+
+--Bai giao them tren lop
+SELECT last_name, e.department_id, job_id FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id
+WHERE l.city = 'Seattle';
+
+--lay ra tat ca cac employee id va department name cua employee day
+SELECT e.employee_id, d.department_name FROM employees e
+LEFT JOIN departments d ON e.department_id = d.department_id;
+
+--hien thi cac nhan vien co department name = 'Finance' va cac nhan vien co location_id 1700
+SELECT e.employee_id FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE d.department_name = 'Finance' OR d.location_id = 1700;
+
+SELECT e.employee_id FROM employees e
+JOIN departments d ON e.department_id = d.department_id WHERE d.department_name = 'Finance'
+UNION ALL
+SELECT e.employee_id FROM employees e
+JOIN departments d ON e.department_id = d.department_id WHERE d.location_id = 1700;
+
+--question 5
+SELECT last_name, salary FROM employees
+WHERE manager_id IN (
+SELECT employee_id FROM employees WHERE last_name = 'King');
+
+--question 6
+SELECT department_id, last_name, job_id FROM employees
+WHERE department_id IN (
+SELECT department_id FROM departments WHERE department_name = 'Executive');
+
+--question 7
+SELECT employee_id, last_name, salary FROM employees
+WHERE salary >
+(SELECT AVG(salary) FROM employees)
+OR department_id IN
+(SELECT department_id FROM employees WHERE last_name LIKE '%u%');
+
+SELECT employee_id, last_name, salary FROM employees
+WHERE salary >
+(SELECT AVG(salary) FROM employees)
+UNION
+SELECT employee_id, last_name, salary FROM employees
+WHERE department_id IN
+(SELECT department_id FROM employees WHERE last_name LIKE '%u%')
+
+--question 8
+SELECT ROUND(MAX(salary),0) 'Maximum',
+ROUND(MIN(salary),0) 'Minimum',
+ROUND(SUM(salary),0) 'Sum',
+ROUND(AVG(salary),0) 'Average'
+FROM employees;
+
+--question 9
+SELECT UPPER(LEFT(last_name, 1)) + SUBSTRING(LOWER(last_name), 2, LEN(last_name)) AS "Name", LEN(last_name) AS "Length of Last Name"
+FROM employees
+WHERE last_name LIKE 'J%' OR last_name LIKE 'A%' OR last_name LIKE 'M%'
+ORDER BY last_name;
+
+--question 10
+SELECT employee_id, last_name, salary, ROUND(salary*(1+15.5/100), 0) AS "New Salary"
+FROM employees;
+
+--question 11
+SELECT last_name, department_id, CAST(null AS varchar)
+FROM employees 
+UNION 
+SELECT CAST(null AS varchar), department_id, department_name
+FROM departments;
+
+--question 12
+SELECT e.employee_id, e.first_name, e.last_name FROM employees e
+JOIN employees m ON e.manager_id = m.employee_id
+WHERE e.hire_date > m.hire_date
+UNION 
+SELECT e.employee_id, e.first_name, e.last_name FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id
+WHERE city = 'Toronto';
